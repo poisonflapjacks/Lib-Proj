6262b38a4464b0c677d90e03254baa83208af5d8
Micheal Chapa <MichealChapa@my.unt.edu>
2023-05-04 12:51:12 -0500

    Adds snake_case to loan files

--- loan.h
+++ loan.h
@@ -12,29 +12,29 @@ using namespace std;
 
 class loan{
 	private:
-	int loanID;
-	int loanBookID;
-	string loanBookTitle;
-	string loanPatName;
-	int loanPatID;
-	int loanDue;
-	int loanStatus;
+	int loan_ID;
+	int loan_item_ID;
+	int loan_pat_ID;
+	int loan_due;
+	int loan_status;
+	string loan_item_name;
+	string loan_pat_name;
 
 	public:
 	loan(){
-        	loanID=0;
-                loanBookID=0;
-		loanBookTitle=" ";
-                loanPatID=0;
-                loanDue=0;
-                loanStatus=0;
-        }
+		loan_ID=0;
+		loan_item_ID=0;
+		loan_item_name=" ";
+		loan_pat_ID=0;
+		loan_due=0;
+		loan_status=0;
+    }
 
-        loan(int bookID, int patID){
-		loanBookID=bookID;
-                loanPatID=patID;
-		loanStatus=0;
-       }
+    loan(int item_ID, int patID){
+		loan_item_ID=item_ID;
+        loan_pat_ID=patID;
+		loan_status=0;
+    }
 
 	void SetLoanID(int LOANID);
 	int GetLoanID();
@@ -42,8 +42,8 @@ class loan{
 	void SetLoanBookID(int bookID);
 	int GetLoanBookID();
 
-	void SetLoanBookTitle(string title);
-	string GetLoanBookTitle();
+	void SetLoanItemName(string name);
+	string GetLoanItemName();
 
 	void SetLoanPatID(int patID);
 	int GetLoanPatID();
